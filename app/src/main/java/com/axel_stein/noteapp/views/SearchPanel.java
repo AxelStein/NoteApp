277dd3b055c9102494b889c8457ee954b708ce03
diefaust <diefaust_93@mail.ru>
2017-11-25 12:43:50 +0200

    Add search panel

---
+++ app/src/main/java/com/axel_stein/noteapp/views/SearchPanel.java
@@ -0,0 +1,262 @@
+package com.axel_stein.noteapp.views;
+
+import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.support.annotation.Nullable;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.axel_stein.noteapp.R;
+import com.axel_stein.noteapp.utils.KeyboardUtil;
+import com.axel_stein.noteapp.utils.SimpleTextWatcher;
+import com.axel_stein.noteapp.utils.ViewUtil;
+
+import java.util.Locale;
+
+public class SearchPanel extends LinearLayout implements View.OnClickListener {
+
+    private EditText mEditSearch;
+
+    private TextView mTextResultCount;
+
+    private ImageButton mButtonPrev;
+
+    private ImageButton mButtonNext;
+
+    private ImageButton mButtonClose;
+
+    private Callback mCallback;
+
+    private int mCursor;
+
+    private int mResultCount;
+
+    private Runnable mTask;
+
+    private String mCurrentQuery;
+
+    public SearchPanel(Context context) {
+        super(context);
+    }
+
+    public SearchPanel(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SearchPanel(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+
+        final Handler handler = new Handler(Looper.getMainLooper());
+        mTask = new Runnable() {
+            @Override
+            public void run() {
+                String query = mEditSearch.getText().toString();
+                if (mCallback != null) {
+                    mCallback.onQueryTextChange(query);
+                }
+            }
+        };
+
+        mEditSearch = findViewById(R.id.edit_search);
+        mEditSearch.addTextChangedListener(new SimpleTextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (!TextUtils.equals(s, mCurrentQuery)) {
+                    mCurrentQuery = s.toString();
+
+                    handler.removeCallbacks(mTask);
+                    handler.postDelayed(mTask, 500);
+                }
+            }
+        });
+        mEditSearch.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    v.clearFocus();
+                    SearchPanel.this.requestFocus();
+                    KeyboardUtil.hide(v);
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        mTextResultCount = findViewById(R.id.text_result_count);
+
+        mButtonPrev = findViewById(R.id.button_prev);
+        mButtonPrev.setOnClickListener(this);
+
+        mButtonNext = findViewById(R.id.button_next);
+        mButtonNext.setOnClickListener(this);
+
+        mButtonClose = findViewById(R.id.button_close);
+        mButtonClose.setOnClickListener(this);
+
+        setQueryResultCount(mResultCount);
+        setCursor(mCursor);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_prev:
+                setCursor(mCursor - 1);
+                if (mCallback != null) {
+                    mCallback.onCursorChange(mCursor);
+                }
+                break;
+
+            case R.id.button_next:
+                setCursor(mCursor + 1);
+                if (mCallback != null) {
+                    mCallback.onCursorChange(mCursor);
+                }
+                break;
+
+            case R.id.button_close:
+                ViewUtil.hide(this);
+
+                mEditSearch.setText(null);
+                mEditSearch.clearFocus();
+                requestFocus();
+                KeyboardUtil.hide(v);
+
+                if (mCallback != null) {
+                    mCallback.onClose();
+                }
+                break;
+        }
+    }
+
+    public void show() {
+        ViewUtil.show(this);
+        mEditSearch.requestFocus();
+        KeyboardUtil.show(mEditSearch);
+    }
+
+    public void setCallback(Callback callback) {
+        mCallback = callback;
+    }
+
+    public void setQueryResultCount(int resultCount) {
+        if (resultCount >= 0) {
+            mResultCount = resultCount;
+            setCursor(resultCount == 0 ? 0 : 1);
+            if (mCursor > 0 && mCallback != null) {
+                mCallback.onCursorChange(mCursor);
+            }
+            ViewUtil.enable(resultCount > 1, mButtonPrev, mButtonNext);
+        }
+    }
+
+    private void setCursor(int cursor) {
+        if (cursor <= 0) {
+            cursor = mResultCount;
+        } else if (cursor > mResultCount) {
+            cursor = mResultCount > 0 ? 1 : 0;
+        }
+
+        mCursor = cursor;
+        mTextResultCount.setText(String.format(Locale.ROOT, "%d/%d", mCursor, mResultCount));
+    }
+
+    public int getCursor() {
+        return mCursor;
+    }
+
+    public int getResultCount() {
+        return mResultCount;
+    }
+
+    static class SavedState extends BaseSavedState {
+        int cursor;
+        int resultCount;
+        boolean shown;
+        String currentQuery;
+
+        SavedState(Parcelable superState) {
+            super(superState);
+        }
+
+        private SavedState(Parcel in) {
+            super(in);
+            cursor = in.readInt();
+            resultCount = in.readInt();
+            shown = in.readByte() != 0;
+            currentQuery = in.readString();
+        }
+
+        @Override
+        public void writeToParcel(Parcel out, int flags) {
+            super.writeToParcel(out, flags);
+            out.writeInt(cursor);
+            out.writeInt(resultCount);
+            out.writeByte((byte) (shown ? 1 : 0));
+            out.writeString(currentQuery);
+        }
+
+        static final Parcelable.Creator<SavedState> CREATOR
+                = new Parcelable.Creator<SavedState>() {
+            public SavedState createFromParcel(Parcel in) {
+                return new SavedState(in);
+            }
+
+            public SavedState[] newArray(int size) {
+                return new SavedState[size];
+            }
+        };
+    }
+
+    @Override
+    public Parcelable onSaveInstanceState() {
+        Parcelable superState = super.onSaveInstanceState();
+        SavedState ss = new SavedState(superState);
+
+        ss.cursor = mCursor;
+        ss.resultCount = mResultCount;
+        ss.shown = ViewUtil.isShown(this);
+        ss.currentQuery = mCurrentQuery;
+
+        return ss;
+    }
+
+    @Override
+    public void onRestoreInstanceState(Parcelable state) {
+        SavedState ss = (SavedState) state;
+        mCurrentQuery = ss.currentQuery;
+
+        super.onRestoreInstanceState(ss.getSuperState());
+
+        setQueryResultCount(ss.resultCount);
+        setCursor(ss.cursor);
+        ViewUtil.show(ss.shown, this);
+    }
+
+    public interface Callback {
+
+        void onQueryTextChange(String q);
+
+        void onCursorChange(int cursor);
+
+        void onClose();
+
+    }
+
+}
